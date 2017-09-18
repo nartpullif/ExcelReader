2edb390a3e9e378036f8aa28df46c14d4e86a321
icyfillup <phil10tran@gmail.com>
2017-09-17 21:06:22 -0700

    completed parsing the referral excel and created the sturcture to hold the data. did the same thing for the va provider network excel

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,38 +1,39 @@
 
-import java.io.File;
 import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
-import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.xml.sax.SAXException;
 
 public class MainActivity
 {
-	public static void main(String[] args) throws IOException, OpenXML4JException, SAXException
+	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		// File myFile = new File("D:\\Google_Drive\\Current Term Work\\CS
-		// Senior Design\\QTC\\sample data\\Sample ERRA Data.xlsx");
 		ERRA erra = null;
+		Referral referral = null;
+		VaProviderNetwork vpNetwork = null;
 		try
 		{
-		erra = new ERRA(
-					"D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample ERRA Data.xlsx");
+			erra = new ERRA("D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample ERRA Data.xlsx");
+			referral = new Referral("D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample referral data v2.xlsx");
+			vpNetwork = new VaProviderNetwork ("D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\VA Provider Network List_non VetFed_083117.xlsx");
 		}
 		catch (IOException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		catch (SAXException e)
 		{
-			File xlsxFile = new File("D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample referral data v2.xlsx");
-			OPCPackage pkg = OPCPackage.open(xlsxFile, PackageAccess.READ);
-			XlsxReferralParser parser = new XlsxReferralParser(pkg);
-			parser.process();
-			pkg.close();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
+		catch (OpenXML4JException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("DONE");	
 	}
 }
