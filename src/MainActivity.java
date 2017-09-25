5b7713f3650e3c5fb39968a90a487861f496f0df
icyfillup <phil10tran@gmail.com>
2017-09-24 23:57:59 -0700

    mapping of Specialty group and VA provider specialty has been added but not doing anything

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
@@ -16,18 +17,20 @@ public class MainActivity
 		VaProviderNetwork vpNetwork = null;
 		try
 		{
-			/*
+			
 			erra = new ERRA(
 					"D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample ERRA Data");
 			referral = new Referral(
 					"D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample referral data v2.xlsx");
 			vpNetwork = new VaProviderNetwork(
 					"D:\\My Document\\My stuff\\Google Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\VA Provider Network List_non VetFed_083117.xlsx");
-			*/
 			
+			
+			/*
 			erra = new ERRA("D:\\Google_Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample ERRA Data.xlsx");
 			referral = new Referral("D:\\Google_Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\Sample referral data v2.xlsx");
 			vpNetwork= new VaProviderNetwork("D:\\Google_Drive\\Current Term Work\\CS Senior Design\\QTC\\sample data\\VA Provider Network List_non VetFed_083117.xlsx");
+			*/
 		}
 		catch (IOException e)
 		{
@@ -44,13 +47,24 @@ public class MainActivity
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		HashMap<String, String[]> specialtyList = new HashMap<>();
+		specialtyList.put("Audio", new String[]{"AUD"});
+		specialtyList.put("Dental", new String[]{"DEN"});
+		specialtyList.put("Eye", new String[]{"OPH"});
+		specialtyList.put("GYN", new String[]{"GEN", "GYN"});
+		specialtyList.put("Mental Health", new String[]{"PSY"});
+		specialtyList.put("Sleep", new String[]{"GEN", "GYN"});
+		specialtyList.put("TBI", new String[]{});
+		specialtyList.put("Other", new String[]{"CAR", "DER", "GAS", "NES", "NEU", "ORT", "OTO", "PUL", "RAD", "URO"});
+		specialtyList.put("N/A", new String[]{"DIA"});
 
 		// System.out.println("DONE");
 
-		MainMenu(erra, referral, vpNetwork);
+		MainMenu(erra, referral, vpNetwork, specialtyList);
 	}
 
-	public static void MainMenu(ERRA erra, Referral referral, VaProviderNetwork vpNetwork)
+	public static void MainMenu(ERRA erra, Referral referral, VaProviderNetwork vpNetwork, HashMap<String, String[]> specialtyList)
 	{
 		boolean isRunning = true;
 		Scanner reader = new Scanner(System.in); // Reading from System.in
@@ -135,8 +149,41 @@ public class MainActivity
 			}
 
 		}
-		
-
 		reader.close();
+	}
+	
+	public static String [] getSpeciatyGroup(HashMap<String, String[]> specialtyList, String providerSpecialty)
+	{
+		String[] Result = specialtyList.get(providerSpecialty);
+		return Result;
+	}
+	
+	public static String getProviderSpecialty(HashMap<String, String[]> specialtyList, String speciatyGroup)
+	{
+		String Result = null;
+		for(String key: specialtyList.keySet())
+		{
+			String[] listOfSpecialty = specialtyList.get(key);
+			for(String Specialty: listOfSpecialty)
+			{
+				if(Specialty.equals(speciatyGroup))
+				{
+					Result = key;
+					break;
+				}
+			}
+			
+			if(!Result.isEmpty())
+			{
+				break;
+			}
+		}
+		
+		if(Result.isEmpty())
+		{
+			System.out.println("getProviderSpecialty(): there should be a positive 'Result' output");
+			System.exit(1);
+		}
+		return Result;
 	}
 }
